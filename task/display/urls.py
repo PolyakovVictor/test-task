d2ae6d840a6f2d3b55c04859acbecacffced1b45
Victor Polyakov <polyakov.victor21@gmail.com>
2023-10-18 22:01:33 +0300

    i think it's done

--- task/display/urls.py
+++ task/display/urls.py
@@ -7,6 +7,10 @@ app_name = 'display'
 urlpatterns = [
     path('', views.user_views, name='users'),
     path('add_user/', views.add_user_views, name='add_user'),
+    path('user_edit/<int:user_id>/', views.user_edit_views, name='user_edit'),
+    path('user_delete/<int:user_id>/', views.user_delete_views, name='user_delete'),
     path('groups/', views.group_views, name='groups'),
     path('add_group/', views.add_group_views, name='add_group'),
+    path('group_edit/<int:group_id>/', views.group_edit_views, name='group_edit'),
+    path('group_delete/<int:group_id>/', views.group_delete_views, name='group_delete'),
 ]
--- task/display/views.py
+++ task/display/views.py
@@ -1,30 +1,65 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User, Group
+from django.shortcuts import redirect, render
+from .models import User, Group
 
 
 def user_views(request):
-    users_data = User.objects.all()
-    context = {
-        'users': []
-    }
-
-    for user in users_data:
-        groups = user.groups.all()
-        groups_list = list(groups.values_list('name', flat=True))
-        user_data = {
-            'id': user.pk,
-            'username': user.username,
-            'groups': ', '.join(groups_list),
-            'date_joined': user.date_joined
-        }
-        context['users'].append(user_data)
-
-    print('context users: ', context)
-    return render(request, "display/users.html", context)
+    users = User.objects.all()
+    return render(request, "display/users.html", {'users': users})
 
 
 def add_user_views(request):
-    return render(request, 'display/add_user.html')
+    groups = Group.objects.all()
+    context = {
+        'groups': groups
+    }
+
+    group = Group.objects.first()
+
+    if group is None:
+        return render(request, 'display/add_group.html', {'errors': 'First you have to create a group'})
+
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        group = request.POST['group']
+        try:
+            user = User.objects.get(username=username)
+            context['errors'] = 'A user with this name already exists'
+        except User.DoesNotExist:
+            user = User.objects.create(username=username, group=Group.objects.get(id=group))
+            user.save()
+
+            return redirect('display:users')
+    return render(request, 'display/add_user.html', context)
+
+
+def user_edit_views(request, user_id):
+    user = User.objects.get(pk=user_id)
+    groups = Group.objects.all()
+
+    context = {
+        'user': user,
+        'groups': groups
+    }
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        group = request.POST['group']
+
+        user.username = username
+        user.group = Group.objects.get(name=group)
+        user.save()
+
+        return redirect('display:users')
+
+    return render(request, 'display/user_edit.html', context)
+
+
+def user_delete_views(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.delete()
+
+    return redirect('display:users')
 
 
 def group_views(request):
@@ -36,13 +71,57 @@ def group_views(request):
         group_data = {
             'id': group.pk,
             'name': group.name,
-            'description': group.group_description,
+            'description': group.description,
         }
 
         context['groups'].append(group_data)
-        print(context)
     return render(request, 'display/groups.html', context)
 
 
 def add_group_views(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+
+        if Group.objects.filter(name=name).exists():
+            context = {
+                'errors': 'A group with this name already exists.'
+            }
+            return render(request, 'display/add_group.html', context)
+
+        group = Group.objects.create(name=name, description=description)
+        group.save()
+
+        return redirect('display:groups')
+
     return render(request, 'display/add_group.html')
+
+
+def group_edit_views(request, group_id):
+    group = Group.objects.get(pk=group_id)
+
+    context = {
+        'group': group,
+    }
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+
+        group.name = name
+        group.description = description
+        group.save()
+
+        return redirect('display:groups')
+
+    return render(request, 'display/group_edit.html', context)
+
+
+def group_delete_views(request, group_id):
+    group = Group.objects.get(pk=group_id)
+
+    if not User.objects.filter(group=group).exists():
+        group.delete()
+        return redirect('display:groups')
+    else:
+        return redirect('display:groups')
